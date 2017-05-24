0def789cddd4227150f552612a07e1156804ac72
Nipun <1.madvoid@gmail.com>
2017-05-24 01:52:58 -0600

    Fixed concatenating bug

--- fileConcatenate.py
+++ fileConcatenate.py
@@ -15,28 +15,28 @@ if __name__ == "__main__":
     fileList = os.listdir(path='.')
     fileList.sort()
 
+    # Filter file list so only LEMS data files remain
+    fileList = [i for i in fileList if i[0] == 'L' and 'CSV' in i and 'Latest' not in i]
+
     # Setup variables
-    fileGroup = []
-    currentGroup = []
-    currentLems = fileList[0][4]
+    fileGroup = []                  # List of lists. Each list is a set of files from one LEMS
+    currentGroup = []               # Used to make file group
+    currentLems = fileList[0][4]    # Start LEMS
 
     # Group files into list of lists. Each sublist has files from one LEMS
     for dataFile in fileList:
         if dataFile[4] == currentLems:
             currentGroup.append(dataFile)
         else:
-            if dataFile[0] != '.':
-                fileGroup.append(currentGroup)
-                currentGroup = [dataFile]
-                currentLems = dataFile[4]
+            fileGroup.append(currentGroup)
+            currentGroup = [dataFile]
+            currentLems = dataFile[4]
 
     fileGroup.append(currentGroup)  # For last group
 
     # Iterate through groups and cat CSV files
     for group in fileGroup:
         # Prepare file to write
-        if (group[0][-3:] != "CSV") or ('LEMS' not in group[0]):
-            continue
         currentLems = group[0][4]
         outFileName = "LEMS"+currentLems+"_Latest.CSV"
         print("Creating", outFileName)
@@ -48,14 +48,12 @@ if __name__ == "__main__":
 
         # Write rest of files, skip header
         for dataFile in group[1:]:
-            if ('Latest' in dataFile):
-                continue
             print("    Opening", dataFile, "and appending to", outFileName)
             inFile = open(dataFile)
             for index, line in enumerate(inFile):
                 if (index == 0):
                     continue
                 fOut.write(line)
-            inFile.close()  # Close writee file
-        fOut.close()    # Close outfile
+            inFile.close()  # Close original file
+        fOut.close()    # Close outfile (concatenated file)
         
